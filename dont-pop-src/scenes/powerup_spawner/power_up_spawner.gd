d5c271f377c7b4cabc605e41007a0045f27037fd
Dewald Els <dewaldifels@gmail.com>
2025-03-11 00:51:34 +0100

    feat: Try again, bomb

--- dont-pop-src/scenes/powerup_spawner/power_up_spawner.gd
+++ dont-pop-src/scenes/powerup_spawner/power_up_spawner.gd
@@ -1,8 +1,55 @@
 class_name PowerUpSpawner
 extends Node2D
 
+
 @export var powerups: Array[PackedScene] = []
+@export var powerup_container: Node
+
+
+@onready var spawn_timer: Timer = %SpawnTimer
+
+var player: Player
+
+var safe_margins: Dictionary = {
+	"start_x" = 40,
+	"end_x" =  600,
+	"start_y" = 40,
+	"end_y" = 350
+}
 
 func _ready() -> void:
-	pass
+	player = get_tree().get_first_node_in_group("player")
+	spawn_timer.timeout.connect(on_spawn_timeout)
 	
+func start_spawn_timer() -> void:
+	spawn_timer.wait_time = get_random_spawn_timeout()
+	spawn_timer.start()
+	
+func get_random_spawn_timeout() -> float:
+	randomize()
+	return randf_range(10, 25)
+	
+func get_random_spawn_position() -> Vector2:
+	# Check not to spawn on top of balloon.
+	randomize()
+	var x: float = randf_range(
+		safe_margins.start_x,
+		safe_margins.end_x
+	)
+	var y: float = randf_range(
+		safe_margins.start_y,
+		safe_margins.end_y
+	)
+	
+	return Vector2(x, y)
+	
+	
+func spawn_random_hazard() -> void:
+	if player:
+		var powerup: BasePowerUp = powerups.pick_random().instantiate()
+		powerup_container.add_child(powerup)
+		powerup.global_position = get_random_spawn_position()
+	
+func on_spawn_timeout() -> void:
+	spawn_random_hazard()
+	start_spawn_timer()
--- dont-pop-src/scenes/screens/try_again/try_again_screen.gd
+++ dont-pop-src/scenes/screens/try_again/try_again_screen.gd
@@ -1,15 +1,20 @@
 class_name TryAgainScreen
 extends CanvasLayer
 
-
+@onready var give_up_button: Button = %GiveUpButton
 @onready var try_again_button: Button = %TryAgainButton
 @onready var score_label: Label = %ScoreLabel
 
 
 func _ready() -> void:
+	give_up_button.pressed.connect(on_give_up_click)
 	try_again_button.pressed.connect(on_try_again_pressed)
 	score_label.text = str(ScoreManager.score)
 	
 func on_try_again_pressed() -> void:
 	ScoreManager.reset_score()
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
+
+func on_give_up_click() -> void:
+	ScoreManager.reset_score()
+	get_tree().change_scene_to_file("res://scenes/screens/main_menu/main_menu_screen.tscn")
